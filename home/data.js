4479283666e94a275c2e23af18f2e62392a17347
xiaoyunzhen <157570032+xiaoyunzhen@users.noreply.github.com>
2024-03-27 14:43:57 +0800

    0

---
+++ home/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.12648160049021584,
+        "pitch": 0.094921757759721,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
